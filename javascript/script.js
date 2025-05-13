add993f229d7ebcc36b69629be24ecd4f7426924
Katsuhkay <alex_magalhaes13@hotmail.com>
2025-05-13 15:42:44 -0300

    Last commit
    Finalizado validação do formulário, algumas animações e otimizações!

--- javascript/script.js
+++ javascript/script.js
@@ -17,3 +17,91 @@ $(document).mouseup(function(e) {
     }
 });
 
+$(document).ready(function(){
+            $('#carousel-images').slick({
+                autoplay: true,
+                dots: true,
+                arrows: true,
+                infinite: true,
+                speed: 500,
+                slidesToShow: 1,
+                slidesToScroll: 1
+        });
+
+            $('.menu-hamburger').click(function(){
+                $('nav').slideToggle();
+            });
+
+            $('#telefone').mask('(00) 00000-0000');
+
+            $('form').validate({
+                rules: {
+                    nome: {
+                        required: true,
+                        minlength: 3
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    telefone: {
+                        required: true
+                    },
+                    'veiculo-de-interesse': {
+                        required: true
+                    },
+                    mensagem: {
+                        required: true,
+                        minlength: 10
+                    }
+                },
+                messages: {
+                    nome: {
+                        required: "Por favor, insira seu nome.",
+                        minlength: "Seu nome deve ter pelo menos 3 caracteres."
+                    },
+                    email: {
+                        required: "Por favor, insira seu e-mail.",
+                        email: "Por favor, insira um e-mail válido."
+                    },
+                    telefone: {
+                        required: "Por favor, insira seu telefone."
+                    },
+                    'veiculo-de-interesse': {
+                        required: "Por favor, informe o veículo de interesse."
+                    },
+                    mensagem: {
+                        required: "Por favor, escreva uma mensagem.",
+                        minlength: "Sua mensagem deve ter pelo menos 10 caracteres."
+                    }
+                },
+                submitHandler: function(form) {
+                    console.log(form);
+                },
+                invalidHandler: function(event, validador) {
+                    let camposInvalidos = validador.numberOfInvalids();
+                    console.log("Número de campos inválidos: " + camposInvalidos);
+                    if (camposInvalidos) {
+                        alert("Por favor, preencha todos os campos obrigatórios corretamente.");
+                    }
+                }
+            });
+
+            $('.lista-veiculos button').click(function() {
+                const destino = $('#contato');
+
+                const nomeVeiculo = ($(this).parent().find('h3').text());
+
+                $('#veiculo-de-interesse').val(nomeVeiculo);
+
+                $('html').animate({
+                    scrollTop: destino.offset().top
+                }, 1000, function() {
+                    destino.addClass('highlight');
+                    setTimeout(function() {
+                        destino.removeClass('highlight');
+                    }, 2000);
+                })
+
+            });
+        });
